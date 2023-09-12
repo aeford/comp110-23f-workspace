9eb4742768e826785126f17045e53af813918e86
Alexis Ford <aeford@unc.edu>
2023-09-12 12:35:38 -0400

    progress on exercise 1

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,2 +1,3 @@
+"""COMP110 First Coding Assignment"""
 print("Hello world, my name is Alexis")
 __author__ = "730566761"
---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,6 @@
+"""EX)! - Chardle - A cute step toward Wordle."""
+__author__ = "730566761"
+
+five_character_word: str = input("Enter a 5-character word: ")
+single_character: str = input("Enter a single character: ")
+print("Searching for e in hello")
